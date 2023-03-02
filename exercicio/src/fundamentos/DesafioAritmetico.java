f08f197da6518958d0faf40012e6a69a8d661346
José Nobre <jnobrecf@gmail.com>
2023-03-02 09:06:00 -0300

    :books: Atualização Aula 42

--- exercicio/src/fundamentos/DesafioAritmetico.java
+++ exercicio/src/fundamentos/DesafioAritmetico.java
@@ -2,4 +2,13 @@ package fundamentos;
 
 public class DesafioAritmetico {
 
+	public static void main(String[] args) {
+		
+		int 	a = 2 + 3 * 4;
+		int 	b = (int) Math.pow(a, 3);
+		double 	c = Math.pow(a, 3);
+		
+		System.out.println(b);
+		System.out.println(c);
+	}
 }
