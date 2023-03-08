104c2f9040744f780ffda9db0b8f4d8c63fb774c
José Nobre <jnobrecf@gmail.com>
2023-03-07 21:53:49 -0300

    :books: Aula: 72 - Do/While

---
+++ exercicio/src/controle/DoWhile.java
@@ -0,0 +1,26 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		// if(...) sentença; ou {}
+		//while(...) sentença; ou {}
+		//for(...;...;...)  sentença; ou {}
+		//do setença; {} ou while(...);
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String texto = "por favor";
+		
+		do {
+			System.out.println("Você precisa falar as palavras mágicas....");
+			System.out.println("Quer sair?");
+		}while(!texto.equalsIgnoreCase("Por favor"));
+			
+		System.out.println("Obrigado!");
+		entrada.close();
+	}
+}
