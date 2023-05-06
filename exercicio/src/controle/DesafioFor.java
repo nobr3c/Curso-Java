ab21fdc494f31ff19f3c776080a0c7cb5c6a29a3
José Nobre <jnobrecf@gmail.com>
2023-05-06 11:10:26 -0300

    :books: Aula: 78 e 79 - Desafio for e Resolução do Desafio

---
+++ exercicio/src/controle/DesafioFor.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.Iterator;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+		
+		String valor = "#";
+		for(int i =1; i <=5; i++) {
+			System.out.println(valor);
+			valor += "#";
+		}
+		//Versao do Desafio
+		//Não pode usar valor númerico para controlar o laço!
+		
+	
+		for(String v = "#"; !v.equals("#####"); v+= "#") {
+			System.out.println(v);
+			
+		}
+	}
+}
