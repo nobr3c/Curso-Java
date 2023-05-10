6aa72686bcb811f5e88e81b625e3b827f97619bc
José Nobre <jnobrecf@gmail.com>
2023-05-10 20:52:39 -0300

    :books: Classes e Métodos - Aulas 98: Desafio Método e Aula 99: Desafio Método - Resposta

--- exercicio/src/classe/DataTeste.java
+++ exercicio/src/classe/DataTeste.java
@@ -15,7 +15,10 @@ public class DataTeste {
 		d2.mes = 12;
 		d2.ano = 2020;
 		
-		System.out.printf("%d/%d/%d\n",d1.dia, d1.mes, d1.ano);
-		System.out.printf("%d/%d/%d",d2.dia, d2.mes, d2.ano);
+		System.out.printf(d1.obterDataFormatada());
+		System.out.printf(d2.obterDataFormatada());
+		
+		d1.imprimirDataFormatada();
+		d2.imprimirDataFormatada();
 	}
 }
