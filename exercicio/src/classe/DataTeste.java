fe7a741c7e81edd02483592227b2ab2ef6370149
José Nobre <jnobrecf@gmail.com>
2023-05-11 21:14:29 -0300

    :books: Classes e Métodos - Aulas 102 e 103: Desafio Construtor e Resposta

--- exercicio/src/classe/DataTeste.java
+++ exercicio/src/classe/DataTeste.java
@@ -5,17 +5,20 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 7;
+		/*d1.dia = 7;
 		d1.mes = 11;
+		*/
 		d1.ano = 2021;
 				
 		//Inferencia
-		var d2 = new Data();
-		d2.dia = 31;
+		var d2 = new Data(31, 12, 2020);
+		/*/d2.dia = 31;
 		d2.mes = 12;
-		d2.ano = 2020;
+		d2.ano = 2020;*/
 		
-		System.out.printf(d1.obterDataFormatada());
+		String dataFormatada1 = d1.obterDataFormatada();
+		
+		System.out.printf(dataFormatada1);
 		System.out.printf(d2.obterDataFormatada());
 		
 		d1.imprimirDataFormatada();
